3fa9081bb45c7f995eb77bce7c5f64a82d62b1c6
吕浩 <lvhao@xiniunet.com>
2015-06-18 01:11:10 +0800

    添加简单路由

--- study/src/02-CarList/scripts/controller.js
+++ study/src/02-CarList/scripts/controller.js
@@ -1,6 +1,23 @@
+angular.module("carList", [])
+    .config(["$routeProvider", function($routeProvider) {
+        $routeProvider
+            .when("/list", {templateUrl:"list.html", controller : CarListController})
+            .when("/view/:name", {templateUrl:"view.html", controller : CarViewController})
+            .otherwise({redirectTo : 'index.html'});
+    }]);
+
+// 车辆列表控制器
 var CarListController = function($scope, $http) {
     $http.get("data/cars.json").success(function(data) {
         $scope.cars = data;
     });
 };
+
+// 车辆查看控制器
+var CarViewController = function($scope, $routeParams) {
+    $scope.name = $routeParams.name;
+};
+
+
 CarListController.$inject=["$scope", "$http"];
+CarViewController.$inject=["$scope", "$routeParams"];
