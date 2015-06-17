57a9bcede767859bcefe6635bdac065580845a17
吕浩 <lvhao@xiniunet.com>
2015-06-18 00:41:05 +0800

    引入$http,通过$http获取数据

--- study/src/02-CarList/scripts/controller.js
+++ study/src/02-CarList/scripts/controller.js
@@ -1,7 +1,6 @@
 var CarListController = function($scope, $http) {
-    $scope.cars = [
-        {"name": "本田XR-V","minPrice": 12.78,"maxPrice": 16.28},
-        {"name": "长安CS35","minPrice": 7.89,"maxPrice": 9.89},
-        {"name": "瑞风S3","minPrice": 6.58,"maxPrice": 8.48}
-    ];
+    $http.get("data/cars.json").success(function(data) {
+        $scope.cars = data;
+    });
 };
+CarListController.$inject=["$scope", "$http"];
